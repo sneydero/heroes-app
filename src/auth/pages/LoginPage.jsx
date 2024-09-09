7a7273fbcacd377327552ac2def8fc17b4ed3a00
Sneyder Ospina Chavarria <sneyder.ospina@e-hps.com>
2024-09-09 15:53:50 -0500

    Private and Public routes

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -8,9 +8,12 @@ export const LoginPage = () => {
 
   const navigate = useNavigate();
   const onLogin = () => {
+
+    const lastPath = localStorage.getItem('lastPath') || '/'
+
     login('Sneyder Ospina');
     
-    navigate('/', {
+    navigate(lastPath, {
       replace: true
     })
   }
